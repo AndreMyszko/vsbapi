201b2e9e8a89bb852761f5a74d3db79ea4676836
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-12 21:06:36 -0300

    first commit

---
+++ src/main/java/com/vsbrasil/vsbapi/VsbapiApplication.java
@@ -0,0 +1,13 @@
+package com.vsbrasil.vsbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsbapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsbrasil/vsbapi/VsbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsbrasil.vsbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
