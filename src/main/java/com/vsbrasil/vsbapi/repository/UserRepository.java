0e2af9b4de39eae8599d045f33ab5040b3054cf2
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-14 13:21:02 -0300

    update. config nosql injection yet

---
+++ src/main/java/com/vsbrasil/vsbapi/repository/UserRepository.java
@@ -0,0 +1,8 @@
+// package com.vsbrasil.vsbapi.repository;
+
+// import com.vsbrasil.vsbapi.entity.User;
+// import org.springframework.data.repository.CrudRepository;
+
+// public interface UserRepository extends CrudRepository<User, Integer> {
+//     User findByName(String name);
+// }
