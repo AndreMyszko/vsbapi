8ea56d9c8886ea7a827f33f8aadba8173ffb8418
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-14 23:48:18 -0300

    update. user.class

--- src/main/java/com/vsbrasil/vsbapi/entity/User.java
+++ src/main/java/com/vsbrasil/vsbapi/entity/User.java
@@ -5,25 +5,41 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user_tbl")
 public class User {
+    //ATRIBUTES:
     @Id //PK - primarykey
     @GeneratedValue(strategy = GenerationType.AUTO) //AI - autoincrement
     private Integer id;
     private String name;
-    // private String email;
-    // private String password;
-    // private String user_role;
-    // private Boolean active;
+    private String email;
+    private String password;
+    private String user_role;
+    private Boolean active;
 
-
-    //private int age;
-
-    
+    //CONSTRUCTORS:
+    //empty constructor:
     public User() {
     }
-    public User(String name /*int age*/) {
+
+    //all fields constructor:
+    public User(Integer id, String name, String email, String password, String user_role, Boolean active) {
+        this.id = id;
         this.name = name;
-        //this.age = age;
+        this.email = email;
+        this.password = password;
+        this.user_role = user_role;
+        this.active = active;
     }
+
+    //no ID constructor:
+    public User(String name, String email, String password, String user_role, Boolean active) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.user_role = user_role;
+        this.active = active;
+    }
+
+    //GETTERS AND SETTERS:
     public Integer getId() {
         return id;
     }
@@ -37,12 +53,54 @@ public class User {
         this.name = name;
     }
 
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser_role() {
+        return this.user_role;
+    }
+
+    public void setUser_role(String user_role) {
+        this.user_role = user_role;
+    }
+
+    public Boolean isActive() {
+        return this.active;
+    }
+
+    public Boolean getActive() {
+        return this.active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    //TO STRING:
     @Override
     public String toString() {
         return "{" +
             " id='" + getId() + "'" +
             ", name='" + getName() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", user_role='" + getUser_role() + "'" +
+            ", active='" + isActive() + "'" +
             "}";
     }
+
     
 }
