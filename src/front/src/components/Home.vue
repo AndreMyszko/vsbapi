951b1fa14e6d867a3db7da7a0f552061f01a2143
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-23 15:46:46 -0300

    update. vue structure

--- src/front/src/components/Home.vue
+++ src/front/src/components/Home.vue
@@ -1,7 +1,10 @@
 <template>
 
+<div class="container">
   <h1>{{title}}</h1>
-  
+  <h3>Users</h3>
+</div>
+
 </template>
 
 <script>
--- src/front/src/components/TableUser.vue
+++ src/front/src/components/TableUser.vue
@@ -1,71 +1,73 @@
 <template>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-      <th scope="col">Active</th>
-    </tr>
-  </thead>
+<div class="container">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Role</th>
+        <th scope="col">Active</th>
+      </tr>
+    </thead>
 
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Andre Myszko</td>
-      <td>email@email.com</td>
-      <td>admin</td>
-      <td>true</td>
-    </tr>
-  </tbody>
-  <tbody>
-    <tr>
-      <th scope="row">2</th>
-      <td>Mithrandir</td>
-      <td>email@email.com</td>
-      <td>admin</td>
-      <td>true</td>
-    </tr>
-  </tbody>
-  <tbody>
-    <tr>
-      <th scope="row">3</th>
-      <td>Tarkûn</td>
-      <td>email@email.com</td>
-      <td>user</td>
-      <td>false</td>
-    </tr>
-  </tbody>
     <tbody>
-        <tr>
-        <th scope="row">4</th>
-        <td>Olórin</td>
+      <tr>
+        <th scope="row">1</th>
+        <td>Andre Myszko</td>
         <td>email@email.com</td>
-        <td>user</td>
+        <td>admin</td>
         <td>true</td>
-        </tr>
+      </tr>
     </tbody>
     <tbody>
-        <tr>
-        <th scope="row">5</th>
-        <td>Incánus</td>
+      <tr>
+        <th scope="row">2</th>
+        <td>Mithrandir</td>
         <td>email@email.com</td>
-        <td>user</td>
+        <td>admin</td>
         <td>true</td>
-        </tr>
+      </tr>
     </tbody>
     <tbody>
-        <tr>
-        <th scope="row">6</th>
-        <td>Gandalf</td>
+      <tr>
+        <th scope="row">3</th>
+        <td>Tarkûn</td>
         <td>email@email.com</td>
         <td>user</td>
-        <td>true</td>
-        </tr>
+        <td>false</td>
+      </tr>
     </tbody>
-</table>
+      <tbody>
+          <tr>
+          <th scope="row">4</th>
+          <td>Olórin</td>
+          <td>email@email.com</td>
+          <td>user</td>
+          <td>true</td>
+          </tr>
+      </tbody>
+      <tbody>
+          <tr>
+          <th scope="row">5</th>
+          <td>Incánus</td>
+          <td>email@email.com</td>
+          <td>user</td>
+          <td>true</td>
+          </tr>
+      </tbody>
+      <tbody>
+          <tr>
+          <th scope="row">6</th>
+          <td>Gandalf</td>
+          <td>email@email.com</td>
+          <td>user</td>
+          <td>true</td>
+          </tr>
+      </tbody>
+  </table>
+</div>
 
 </template>
 
