4bd9807101b993c9e5640661b897213a63738146
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-03-24 23:43:48 -0300

    add. userService

---
+++ src/front/src/service/UserService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default class UserService {
+
+    url="http://localhost:8082/api";
+
+    getAll(){
+        return axios.get(this.url + "/all-users");
+    }
+}
